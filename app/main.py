5ad43b67eaaf37f83d79a41d67a00a7c330b3783
Raul Sanchez Sanchez <rauulssanchezz@gmail.com>
2026-04-24 15:10:36 +0200

    endpoints fastapi users

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from schemas.user import UserCreate, UserRead, UserUpdate
 from core.deps.fastapi_users import fastapi_users
 from core.auth_backend import auth_backend
 
@@ -14,4 +15,28 @@ app.include_router(
     fastapi_users.get_auth_router(auth_backend),
     prefix="/auth/jwt",
     tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_verify_router(UserRead),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_reset_password_router(),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate, requires_verification=True),
+    prefix="/users",
+    tags=["users"],
 )
