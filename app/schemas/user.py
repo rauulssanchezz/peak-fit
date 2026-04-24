3f1b67959f37686175bee73e8d70df0f08749a7a
Raul Sanchez Sanchez <rauulssanchezz@gmail.com>
2026-04-24 15:54:20 +0200

    goals enum

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,16 +1,24 @@
+from enum import Enum
 import uuid
 from fastapi_users import schemas
 from pydantic import Field
 
+class Goals(str, Enum):
+    GAIN_WEIGHT = "Ganar Peso."
+    LOSE_WEIGHT = "Perder Peso."
+    DEFINITION = "Definir."
+    VOLUME = "Volumen."
+    FIT = "Estar en forma."
+
 
 class UserRead(schemas.BaseUser[uuid.UUID]):
     weight: float = Field(gt=35, lt=400)
-    goal: str = Field(min_length=0, max_length=50)
+    goal: Goals
 
 class UserCreate(schemas.BaseUserCreate):
     weight: float = Field(gt=35, lt=400)
-    goal: str = Field(min_length=0, max_length=50)
+    goal: Goals
 
 class UserUpdate(schemas.BaseUserUpdate):
     weight: float = Field(gt=35, lt=400)
-    goal: str = Field(min_length=0, max_length=50)
+    goal: Goals
