c3f08d65df0710d9ed9dbfbbc6ba9a8d34a2866a
Raul Sanchez Sanchez <rauulssanchezz@gmail.com>
2026-04-24 16:06:50 +0200

    nueva tabla exercise

---
+++ app/models/excercise.py
@@ -0,0 +1,11 @@
+import uuid
+from sqlalchemy import UUID, String, Text
+from sqlalchemy.orm import Mapped, mapped_column
+from core.db import Base
+
+
+class Exercise(Base):
+    id: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True, default=uuid.uuid4)
+    name: Mapped[str] = mapped_column(String(50))
+    description: Mapped[str] = mapped_column(Text())
+    category: Mapped[str] = mapped_column(String(30))
---
+++ app/schemas/exercise.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+from pydantic import BaseModel, Field
+
+class ExerciseCategory(str, Enum):
+    CHEST = "Chest"
+    BACK = "Back"
+    BICEPS = "Bíceps"
+    TRICEPS = "Triceps"
+
+class ExerciseCreate(BaseModel):
+    name: str = Field(min_length=5, max_length=50)
+    description: str
+    category: ExerciseCategory
