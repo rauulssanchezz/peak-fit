d0fbb82df55bc0a68f17ac39423bd19391f131d0
Raul Sanchez Sanchez <rauulssanchezz@gmail.com>
2026-04-24 15:37:46 +0200

    añadir weight a user

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,8 @@
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID
+from sqlalchemy import Float
+from sqlalchemy.orm import Mapped, mapped_column
 from app.core.db import Base
 
 class User(SQLAlchemyBaseUserTableUUID, Base):
-    pass
+    weight: Mapped[float] = mapped_column(Float(precision=2))
+    weight_goal: Mapped[float] = mapped_column(Float(precision=2))
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,15 +1,18 @@
 import uuid
-
 from fastapi_users import schemas
+from pydantic import Field
 
 
 class UserRead(schemas.BaseUser[uuid.UUID]):
-    pass
+    weight: float = Field(gt=35, lt=400)
+    weight_goal: float = Field(gt=35, lt=400)
 
 
 class UserCreate(schemas.BaseUserCreate):
-    pass
+    weight: float = Field(gt=35, lt=400)
+    weight_goal: float = Field(gt=35, lt=400)
 
 
 class UserUpdate(schemas.BaseUserUpdate):
-    pass
+    weight: float = Field(gt=35, lt=400)
+    weight_goal: float = Field(gt=35, lt=400)
