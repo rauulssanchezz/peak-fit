6efd4445866f87160cf1bcd0a47e817e3af6fb29
Raul Sanchez Sanchez <rauulssanchezz@gmail.com>
2026-04-24 15:39:52 +0200

    arreglar imports

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,7 @@
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID
 from sqlalchemy import Float
 from sqlalchemy.orm import Mapped, mapped_column
-from app.core.db import Base
+from core.db import Base
 
 class User(SQLAlchemyBaseUserTableUUID, Base):
     weight: Mapped[float] = mapped_column(Float(precision=2))
